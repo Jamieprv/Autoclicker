a74d13d6af95df838dd78b0506466b3853314b31
Jamie <115030926+Jamieprv@users.noreply.github.com>
2022-12-03 14:17:38 +0100

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -35,7 +35,7 @@ local Text = Draw("Text", {
     Outline = true,
     OutlineColor = Color3.fromRGB(255, 255, 255),
     Color = Color3.fromRGB(0, 0, 0),
-    Text = "Auto Clicking : FALSE",
+    Text = "Auto Clicking : FALSE\nMouse Locked : FALSE",
     Visible = true,
 })
 --Key Bind--
@@ -52,7 +52,17 @@ end)
 coroutine.wrap(function()
     while (true) do
         Text.Visible = Settings.GUI
-        Text.Position = Vector2.new(Camera.ViewportSize.X - 133, Camera.ViewportSize.Y - 32)
+        Text.Position = Vector2.new(Camera.ViewportSize.X - 133, Camera.ViewportSize.Y - 22)
+
+        if (flags.Auto_Clicking) then
+            for i = 1, 2 do
+                if (flags.Mouse_Locked) then
+                    VIM:SendMouseButtonEvent(flags.Mouse_Locked_Position.X, flags.Mouse_Locked_Position.Y, Settings["Right Click"] and 1 or 0, i == 1, nil, 0)
+                else
+                    VIM:SendMouseButtonEvent(Mouse.X, Mouse.Y, Settings["Right Click"] and 1 or 0, i == 1, nil, 0)
+                end
+            end
+        end
 
         if (Settings.Delay <= 0) then
             RunService.RenderStepped:Wait()
